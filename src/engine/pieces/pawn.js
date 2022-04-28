080863324142acf508f9d521cdd7345026db07ae
Rachelle Dupuis <Rachelle.Dupuis@softwire.com>
2022-04-28 17:15:06 +0100

    Allow pawns to move up or down one square

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Player from "../player";
+import Square from "../square";
 
 export default class Pawn extends Piece {
     constructor(player) {
@@ -6,6 +8,11 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this);
+        if (this.player === Player.WHITE) {
+            return Square.at(location.row + 1, location.col);
+        } else {
+            return Square.at(location.row - 1, location.col);
+        }
     }
 }
